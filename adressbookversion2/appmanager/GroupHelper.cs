b830cf50c1eca43b913c765c4fd29e36a9f5a810
nataliya.kachmasheva <nataliya.kachmasheva@IPRW053>
2019-06-20 12:39:39 +0400

    2.5

--- adressbookversion2/appmanager/GroupHelper.cs
+++ adressbookversion2/appmanager/GroupHelper.cs
@@ -29,12 +29,28 @@ namespace adressbook_web_tests
             return this;
         }
 
+        public GroupHelper Modify(int p, GroupData newData)
+        {
+            manager.Navigator.GoToGroupsPage();
+            SelectGroup(p);
+            InitGroupModification();
+            FillGroupForm(newData);
+            SubmitGroupModification();
+            ReturnToGroupsPage();
+
+            return this;
+        }
+
+        
+
         public GroupHelper Remove(int p)
         {
             manager.Navigator.GoToGroupsPage();
-            SelectGroup(1);
+
+            SelectGroup(p);
             RemoveGroup();
             ReturnToGroupsPage();
+
             return this;
         }
 
@@ -86,6 +102,17 @@ namespace adressbook_web_tests
             return this;
         }
 
-       
+        public GroupHelper SubmitGroupModification()
+        {
+            driver.FindElement(By.Name("update")).Click();
+            return this;
+        }
+
+        public GroupHelper InitGroupModification()
+        {
+            driver.FindElement(By.Name("edit")).Click();
+            return this;
+        }
+
     }
 }
