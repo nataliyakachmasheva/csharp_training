bfa58c0978041053260a7e7e3048997f1709a7bb
nataliya.kachmasheva <nataliya.kachmasheva@IPRW053>
2019-07-01 11:24:33 +0400

    4.0.1

---
+++ adressbookversion2/tests/UnitTest2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace adressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+            string[] s = new string[] { "test1",  "test2", "test3" , "test4" }; 
+
+            foreach (string element in s)
+            {
+                System.Console.Out.Write(element + "\n");
+            }
+        }
+    }
+}
