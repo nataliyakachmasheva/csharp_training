0ad1840f4403cb38df360784e7b866e2a1707a32
nataliya.kachmasheva <nataliya.kachmasheva@IPRW053>
2019-07-08 16:51:15 +0400

    5.2

--- adressbookversion2/appmanager/LoginHelper.cs
+++ adressbookversion2/appmanager/LoginHelper.cs
@@ -45,9 +45,15 @@ namespace adressbook_web_tests
         public bool IsLoggedIn(AccountData account)
         {
             return IsLoggedIn()
-                && driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text
-                    == "(" +account.Username+ ")";
+                && GetLoggetUserName() == account.Username;
+                
            
+        }
+
+        public string GetLoggetUserName()
+        {
+            string text =  driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text;
+            return text.Substring(1, text.Length - 2);
         }
 
         public void Logout()
