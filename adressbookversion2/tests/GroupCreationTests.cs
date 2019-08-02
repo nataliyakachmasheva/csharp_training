7040772c2012c9e627d638182092417b5a2db36c
nataliya.kachmasheva <nataliya.kachmasheva@IPRW053>
2019-08-02 10:27:44 +0400

    6.1.1 создание файла

--- adressbookversion2/tests/GroupCreationTests.cs
+++ adressbookversion2/tests/GroupCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -27,9 +28,25 @@ namespace adressbook_web_tests
             return groups;
         }
 
-       
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+                });
+            }
+            return groups;
+        }
+
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             
